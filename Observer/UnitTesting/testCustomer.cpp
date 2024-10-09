65fa9e70b7dcb69b33500a661e99e8e612f3ab42
dakom35 <alfred.lalanne@gmail.com>
2024-10-09 18:18:16 +0200

    removed useless name argument in test_constructor_name_empty - added Doxygen style documentations to the different methods

--- Observer/UnitTesting/testCustomer.cpp
+++ Observer/UnitTesting/testCustomer.cpp
@@ -8,13 +8,24 @@ int main(void){
     GroceryStore *groceryStore = new GroceryStore(itemList);
     bool res = test_constructor_name_pass(groceryStore,"John ColdTrain");
     interpret_test(true,res,0);
-    res = test_constructor_name_empty(groceryStore,"");
+    res = test_constructor_name_empty(groceryStore);
     interpret_test(true,res,1);
 
 
 }
 
 // Constructor tests
+
+
+/*  @brief Tests the case where a valid name is used to initialize the Customer instance via the constructor
+*
+*   @param groceryStore pointer to the groceryStore (publisher/subject) used
+*   @param name string that should contain a valid name to initialize the Customer instance via its constructor. 
+*   @return bool True if the name entered in the constructor is actually defining the name_ attribute of the customer instance. 
+*
+*   @warning You shouldn't use this test to see what happens when you use an empty string. test_constructor_name_empty() is there for that.
+*   
+*/ 
 bool test_constructor_name_pass(GroceryStore *groceryStore, std::string name){
     Customer *customer = new Customer(*groceryStore,name);
     if(customer->getName() == name){
@@ -25,8 +36,15 @@ bool test_constructor_name_pass(GroceryStore *groceryStore, std::string name){
     }
 }
 
-bool test_constructor_name_empty(GroceryStore *groceryStore, std::string name){
-    Customer *customer = new Customer(*groceryStore,name);
+
+/*  @brief Tests the case where an empty string is used for the name in the constructor
+*
+*   @param groceryStore pointer to the groceryStore (publisher/subject) used
+*   @return bool True if the getter returns "AnonymousCustomer". Else, false.
+*   
+*/ 
+bool test_constructor_name_empty(GroceryStore *groceryStore){
+    Customer *customer = new Customer(*groceryStore,"");
     if(customer->getName() == "AnonymousCustomer"){
         return true ;
     }
@@ -35,7 +53,17 @@ bool test_constructor_name_empty(GroceryStore *groceryStore, std::string name){
     }
 }
 
-
+/*  @brief Interprets the result of a test.
+*
+*   Compares the expected result with the actual result to determine 
+*   if the test is successful or not. 
+*   
+*   @param expected The expected result of the test
+*   @param actual The actual result returned by the test method
+*   @param num The test number
+*   @return void
+*   
+*/ 
 void interpret_test(bool expected, bool actual, int num){
     if(expected == actual){
         std::cout << "TEST #"<<num<<" SUCCESSFUL \n" ;
--- Observer/UnitTesting/testCustomer.h
+++ Observer/UnitTesting/testCustomer.h
@@ -3,7 +3,7 @@
 #include "../Customer.h"
 
 bool test_constructor_name_pass(GroceryStore *groceryStore, std::string name);
-bool test_constructor_name_empty(GroceryStore *groceryStore, std::string name);
+bool test_constructor_name_empty(GroceryStore *groceryStore);
 void interpret_test(bool expected, bool actual, int num);
 
 #endif 
