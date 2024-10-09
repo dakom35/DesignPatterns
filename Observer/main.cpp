2a9f548e796b69b555024421a044c5d3a67a9fff
dakom35 <alfred.lalanne@gmail.com>
2024-10-09 12:10:16 +0200

    initialize GroceryItem structure with a quantity instead of a boolean to adapt to modified structure

--- Observer/main.cpp
+++ Observer/main.cpp
@@ -3,9 +3,12 @@
 #include "Observer.h"
 #include "Subject.h"
 
+// compile me with following command : 
+// g++ -o main.exe main.cpp GroceryStore.cpp Customer.cpp Observer.cpp Subject.cpp
+
 int main() {
-    GroceryItem itemList[] = {{"Flour", true}, {"Rice", true}, {"Brown Bread", false}, \
-                              {"Blueberry Syrup", false}, {"Detergent", true}};   
+    GroceryItem itemList[] = {{"Flour", 10}, {"Rice", 10}, {"Brown Bread", 0}, \
+                              {"Blueberry Syrup", 0}, {"Detergent", 10}};   
     GroceryStore *objGs = new GroceryStore(itemList);
     Customer *objCust = new Customer(*objGs);
     GroceryItem* itemInventory = itemList ;
@@ -16,11 +19,11 @@ int main() {
     objCust->purchaseItem("Brown Bread",itemInventory);
     objCust->purchaseItem("Blueberry Syrup",itemInventory);
 
-    objGs->refillInventory("Brown Bread");
+    objGs->refillInventory("Brown Bread",10);
     objCust->purchaseItem("Brown Bread",itemInventory);
     objCust->unsubscribeCustomer();
 
-    objGs->refillInventory("Blueberry Syrup");
+    objGs->refillInventory("Blueberry Syrup",10);
     
     return 0;
 }
