cd8f5ea9b84e6cac8e99839aa9bc891e792e48dc
dakom35 <alfred.lalanne@gmail.com>
2024-10-09 13:36:52 +0200

    initialize Customer constructor with a name

--- Observer/main.cpp
+++ Observer/main.cpp
@@ -10,7 +10,7 @@ int main() {
     GroceryItem itemList[] = {{"Flour", 10}, {"Rice", 10}, {"Brown Bread", 0}, \
                               {"Blueberry Syrup", 0}, {"Detergent", 10}};   
     GroceryStore *groceryStore = new GroceryStore(itemList);
-    Customer *customer = new Customer(*groceryStore);
+    Customer *customer = new Customer(*groceryStore,"Bjarne");
     GroceryItem* itemInventory = itemList ;
 
     customer->purchaseItem("Detergent", itemInventory);
